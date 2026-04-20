c716638474cfe5daeb1cfa3690b2136a9f6f6f74
Rutaganda <valentinrutaganda04@gmail.com>
2026-04-20 08:18:07 +0000

    Apr 20, 2026, 10:18 AM

--- nutritrack-api/src/main/java/com/nutritrack/model/Users.java
+++ nutritrack-api/src/main/java/com/nutritrack/model/Users.java
@@ -8,7 +8,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 
-// Derived from: RegisterPage, LoginPage, UserManagement, Profile
 @Entity
 @Data
 public class Users {
