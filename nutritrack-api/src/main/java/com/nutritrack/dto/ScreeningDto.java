97457d0e6376958443101ca7cdd4dfb446193644
Rutaganda <valentinrutaganda04@gmail.com>
2026-05-18 10:56:21 +0200

    Change screeningDate to String and remove edema from ScreeningDto

--- nutritrack-api/src/main/java/com/nutritrack/dto/ScreeningDto.java
+++ nutritrack-api/src/main/java/com/nutritrack/dto/ScreeningDto.java
@@ -1,18 +1,15 @@
 package com.nutritrack.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.NotEmpty;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public record ScreeningDto(
         @NotNull Long patientId,
         @NotNull BigDecimal weightKg,
         @NotNull BigDecimal heightCm,
         @NotNull BigDecimal muacCm,
-        boolean edema,
         String appetite,
         String observationNotes,
-        @NotNull LocalDate screeningDate
+        @NotNull String screeningDate
 ) {}
