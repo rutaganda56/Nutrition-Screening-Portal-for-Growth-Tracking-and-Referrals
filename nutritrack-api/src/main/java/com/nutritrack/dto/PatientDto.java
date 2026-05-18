61619cc57ffb1a3d3425b8f1785e178b4e9a35ae
Rutaganda <valentinrutaganda04@gmail.com>
2026-05-18 10:56:05 +0200

    Change birthDate to String to fix Jackson deserialization

--- nutritrack-api/src/main/java/com/nutritrack/dto/PatientDto.java
+++ nutritrack-api/src/main/java/com/nutritrack/dto/PatientDto.java
@@ -2,27 +2,15 @@ package com.nutritrack.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public record PatientDto(
         @NotEmpty String firstName,
         @NotEmpty String lastName,
-        @NotNull @Past LocalDate birthDate,
+        @NotNull String birthDate,
         @NotEmpty String gender,
-        BigDecimal birthWeightKg,
-        BigDecimal birthLengthCm,
         @NotEmpty String guardianFirstName,
         @NotEmpty String guardianLastName,
         @NotEmpty String guardianRelationship,
         @NotEmpty String guardianPhone,
-        String guardianAltPhone,
-        @NotEmpty String village,
-        @NotEmpty String zone,
-        String householdId,
-        @NotEmpty String address,
-        String notes,
-        @NotNull Long facilityId
+        String notes
 ) {}
